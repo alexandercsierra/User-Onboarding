af8e301bb57215d9edc4ff616b416a11d51c521a
alexandercsierra <alexandercsierra@gmail.com>
2019-12-18 22:22:38 -0500

    initial commit

---
+++ useronboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormComp from './components/Form'
+import {Formik, Form, Field} from 'formik'
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
